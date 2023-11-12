88bba972ec0d952de69059f3249dc19aff15b41a
ZZDirty <muztaga2@gmail.com>
2023-11-12 23:38:37 +0800

    initial commit

---
+++ xmake.lua
@@ -0,0 +1,21 @@
+set_project("DiffCloth")
+set_languages("c++17")
+
+if is_mode("release") then
+    set_optimize("fastest")
+end
+
+add_requires("eigen","nanogui","pybind11","openmp", "glad", "glm")
+
+target("DiffCloth")
+    set_kind("binary")
+    add_files("src/code/**.cpp")
+    add_files("src/code/**.c")
+    add_headerfiles("src/code/**.h")
+    add_includedirs("external/LBFGSpp/include")
+
+    add_packages("eigen","nanogui","pybind11","openmp", "glad", "glm")
+
+    add_defines("SOURCE_PATH=\"$(projectdir)\"")
+
+
